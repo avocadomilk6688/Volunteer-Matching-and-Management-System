3df7d9f9bfc94ce72cb4a33bdaf97194b2da2c81
Celeste Ng <ngjinmun123@gmail.com>
2026-04-12 14:02:18 +0800

    Implement users module DTOs

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,33 @@
-export class CreateUserDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  IsEnum,
+  MinLength,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password is too short (min 8 characters)' })
+  password!: string;
+
+  @IsEnum(['admin', 'volunteer', 'organization'])
+  @IsOptional()
+  role?: string;
+}
+
+export class CreateAdminDto {
+  @IsString()
+  @IsNotEmpty()
+  userId!: string;
+}
