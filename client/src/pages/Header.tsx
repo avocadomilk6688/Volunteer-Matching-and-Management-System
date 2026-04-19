d1a6b33febc9de1c22eae4700b09d6ae16036f13
Celeste Ng <ngjinmun123@gmail.com>
2026-04-19 20:20:38 +0800

    Add Header component

---
+++ client/src/pages/Header.tsx
@@ -0,0 +1,9 @@
+import './header.css';
+
+export function Header() {
+    return (
+        <div className="header-container">
+            <h1>Volunteer Matching and Management System</h1>
+        </div>
+    )
+}
